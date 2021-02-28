9eaad3ccc6c12ab9365ab3e03c923344e796cec6
dppsencik <79119774+dppsencik@users.noreply.github.com>
2021-02-28 13:14:55 -0600

    Created SceneFile class and an initial constructor for the object.

---
+++ SceneFile.py
@@ -0,0 +1,16 @@
+# {folder_path}/{descriptor}_{task}_{v###}.{ext}
+#
+
+class SceneFile(object):
+    """An abstract representation of a Scene file."""
+
+    def __init__(self, folder_path, descriptor, task, ver, ext):
+        self.folder_path = folder_path
+        self.descriptor = descriptor
+        self.task = task
+        self.ver = ver
+        self.ext = ext
+
+
+scene_file = SceneFile("C:\\", "tank", "model", "v001", ".ma")
+print(scene_file.descriptor)
