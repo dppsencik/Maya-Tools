0c6a9a9cda7ef3e321a2b98ff4a4e19cb2d614d0
APPsencik <dppsencik@gmail.com>
2021-03-11 17:12:29 -0600

    Added increment_save() and next_available_ver() to allow for incremental save and ensuring the save does not overide possible future saves

--- src/SceneFile.py
+++ src/SceneFile.py
@@ -55,3 +55,32 @@ class SceneFile(object):
             self.folder_path.makedirs_p()
             return pmc.system.saveAs(self.path)
 
+    def next_available_ver(self):
+        """returns the next available version number in the folder."""
+        pattern = "{descriptor}_{task}_v*{ext}".format(
+            descriptor=self.descriptor, task=self.task, ext=self.ext)
+        matching_scenefiles = []
+        for file_ in self.folder_path.files():
+            if file_.name.fnmatch(pattern):
+                matching_scenefiles.append(file_)
+        if not matching_scenefiles:
+            return 1
+        matching_scenefiles.sort(reverse=True)
+        latest_scenefile = matching_scenefiles[0]
+        latest_scenefile = latest_scenefile.name.stripext()
+        latest_ver_num = int(latest_scenefile.split("_v")[-1])
+        return latest_ver_num + 1
+
+    def increment_save(self):
+        """Increments the version and saves the scene file.
+
+        If the existing verstion of an existing file already exists, it should
+        increment from the largest version number available in the folder.
+
+        Returns:
+            Path: the path to the scene file if successful
+        """
+        self.ver = self.next_available_ver()
+        self.save()
+
+
