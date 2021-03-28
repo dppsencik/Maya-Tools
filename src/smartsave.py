90d02d5c304923258579987eb4c011d5cfb030d6
APPsencik <dppsencik@gmail.com>
2021-03-28 16:03:50 -0500

    Added QT functionality to smartsave.py and created a basic header for the window.

--- src/smartsave.py
+++ src/smartsave.py
@@ -1,10 +1,38 @@
-
-import logging
-from pymel.core.system import Path
 import pymel.core as pmc
+import logging
+import maya.OpenMayaUI as omui
+from PySide2 import QtWidgets, QtCore
+from shiboken2 import wrapInstance
+from pymel.core.system import Path
+
 
 log = logging.getLogger(__name__)
 
+
+def maya_main_window():
+    """Return the maya main window widget"""
+    main_window = omui.MQtUtil.mainWindow()
+    return wrapInstance(long(main_window), QtWidgets.QWidget)
+
+
+class SmartSaveUI(QtWidgets.QDialog):
+    """Smart Save UI Class"""
+
+    def __init__(self):
+        super(SmartSaveUI, self).__init__(parent=maya_main_window())
+        self.setWindowTitle("Smart Save UI")
+        self.setMinimumWidth(500)
+        self.setMaximumHeight(200)
+        self.setWindowFlags(self.windowFlags() ^
+                            QtCore.Qt.WindowContextHelpButtonHint)
+        self.create_ui()
+
+    def create_ui(self):
+        self.title_lbl = QtWidgets.QLabel("Smart Save")
+        self.title_lbl.setStyleSheet("font: bold 20px")
+        self.main_lay = QtWidgets.QVBoxLayout()
+        self.main_lay.addWidget(self.title_lbl)
+        self.setLayout(self.main_lay)
 
 class SceneFile(object):
     """An abstract representation of a Scene file."""
