544d127f1f5288c0ac9190e21750b5cbecf57d56
APPsencik <dppsencik@gmail.com>
2021-04-02 22:42:54 -0500

    Now checks if the first selection is an object and instances the selection arbitrarily.

--- src/scattertool.py
+++ src/scattertool.py
@@ -4,9 +4,21 @@ import logging
 
 log = logging.getLogger(__name__)
 
-class Scatter():
+
+class Scatter:
 
     def __init__(self):
         selection = cmds.ls(orderedSelection=True)
-        print selection
-        source = selection[0]
+        self.source = selection[0]
+        # vertices = cmds.filterExpand(sm=31)
+
+        print "Source: %s" % self.source
+
+        if cmds.objectType(self.source) == "transform":
+            self.instance_selection()
+        else:
+            log.warning("Please make sure your first selection is an object.")
+
+    def instance_selection(self):
+        instance_result = cmds.instance(self.source, name=self.source + "_instance%")
+        cmds.move(1, 1, 1, instance_result,)
