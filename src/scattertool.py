a4d2b252261680803c7ec74f5743313db4a111e5
APPsencik <dppsencik@gmail.com>
2021-04-03 01:00:06 -0500

    added function to create random numbers for rotation and scale which have been added to the xform method.

--- src/scattertool.py
+++ src/scattertool.py
@@ -6,12 +6,25 @@ import random as rand
 log = logging.getLogger(__name__)
 
 
+def get_random_numbers(num_range):
+    result = []
+    if num_range[1]-num_range[0] <= 5:
+        for x in range(3):
+            result.append(float('%.2f' % rand.uniform(num_range[0], num_range[1])))
+    else:
+        for x in range(3):
+            result.append(rand.randrange(num_range[0], num_range[1]))
+    return result
+
+
 class Scatter:
 
     def __init__(self):
         selection = cmds.ls(orderedSelection=True)
         self.source = selection[0]
         self.vertices = cmds.filterExpand(sm=31)
+        self.rotateRange = 0, 360                           # Values need to be checked elsewhere
+        self.scaleRange = 1.0, 2.0                          # as to not cause errors with random
         if cmds.objectType(self.source) == "transform":
             self.instance_selection()
         else:
@@ -22,7 +35,14 @@ class Scatter:
         for vert in self.vertices:
             instance_result = cmds.instance(self.source, name=self.source + "_instance#")
             position = cmds.pointPosition(vert, world=True)
-            cmds.xform(instance_result, translation=position)
+            cmds.xform(instance_result,
+                       translation=position,
+                       rotation=get_random_numbers(self.rotateRange),
+                       scale=get_random_numbers(self.scaleRange))
             cmds.parent(instance_result, instance_group)
         cmds.xform(instance_group, centerPivots=True)
+
+
+
+
 
