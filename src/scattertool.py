75b343bc47832c8c33b7f05cfb25ffbac55d1b6a
APPsencik <dppsencik@gmail.com>
2021-04-02 23:41:37 -0500

    Added vertex functionality, now makes multiple instances on vertex locations and groups them.

--- src/scattertool.py
+++ src/scattertool.py
@@ -1,6 +1,7 @@
 import maya.cmds as cmds
 import pymel.core as pmc
 import logging
+import random as rand
 
 log = logging.getLogger(__name__)
 
@@ -10,15 +11,18 @@ class Scatter:
     def __init__(self):
         selection = cmds.ls(orderedSelection=True)
         self.source = selection[0]
-        # vertices = cmds.filterExpand(sm=31)
-
-        print "Source: %s" % self.source
-
+        self.vertices = cmds.filterExpand(sm=31)
         if cmds.objectType(self.source) == "transform":
             self.instance_selection()
         else:
             log.warning("Please make sure your first selection is an object.")
 
     def instance_selection(self):
-        instance_result = cmds.instance(self.source, name=self.source + "_instance%")
-        cmds.move(1, 1, 1, instance_result,)
+        instance_group = cmds.group(empty=True, name=self.source + "_instanceGroup#")
+        for vert in self.vertices:
+            instance_result = cmds.instance(self.source, name=self.source + "_instance#")
+            position = cmds.pointPosition(vert, world=True)
+            cmds.xform(instance_result, translation=position)
+            cmds.parent(instance_result, instance_group)
+        cmds.xform(instance_group, centerPivots=True)
+
