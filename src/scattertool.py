05f7494bc3350186353dcc527fd6ff1df212110f
APPsencik <dppsencik@gmail.com>
2021-04-10 14:49:44 -0500

    Remade random number generators to support 3 channels of rotation and scale individually

--- src/scattertool.py
+++ src/scattertool.py
@@ -1,20 +1,13 @@
-import maya.cmds as cmds
 import pymel.core as pmc
 import logging
+import maya.OpenMayaUI as omui
+import maya.cmds as cmds
+from PySide2 import QtWidgets, QtCore
+from shiboken2 import wrapInstance
 import random as rand
 
+
 log = logging.getLogger(__name__)
-
-
-def get_random_numbers(num_range):
-    result = []
-    if num_range[1]-num_range[0] <= 5:
-        for x in range(3):
-            result.append(float('%.2f' % rand.uniform(num_range[0], num_range[1])))
-    else:
-        for x in range(3):
-            result.append(rand.randrange(num_range[0], num_range[1]))
-    return result
 
 
 class Scatter:
@@ -23,8 +16,8 @@ class Scatter:
         selection = cmds.ls(orderedSelection=True)
         self.source = selection[0]
         self.vertices = cmds.filterExpand(sm=31)
-        self.rotateRange = 0, 360                           # Values need to be checked elsewhere
-        self.scaleRange = 1.0, 2.0                          # as to not cause errors with random
+        self.rotateRanges = [[0, 0], [0, 360], [0, 0]]
+        self.scaleRanges = [[1.0, 2.0], [1.0, 2.0], [1.0, 2.0]]
         if cmds.objectType(self.source) == "transform":
             self.instance_selection()
         else:
@@ -37,10 +30,30 @@ class Scatter:
             position = cmds.pointPosition(vert, world=True)
             cmds.xform(instance_result,
                        translation=position,
-                       rotation=get_random_numbers(self.rotateRange),
-                       scale=get_random_numbers(self.scaleRange))
+                       rotation=self.get_rotate_range(),
+                       scale=self.get_scale_range())
             cmds.parent(instance_result, instance_group)
         cmds.xform(instance_group, centerPivots=True)
+
+    def get_rotate_range(self):
+        rotation = []
+        for r in self.rotateRanges:
+            if r[0] == r[1]:
+                rotation.append(r[0])
+            else:
+                rotation.append(rand.randrange(r[0], r[1]))
+
+        return rotation
+
+    def get_scale_range(self):
+        scale = []
+        for r in self.scaleRanges:
+            if r[0] == r[1]:
+                scale.append(r[0])
+            else:
+                scale.append(rand.uniform(r[0], r[1]))
+
+        return scale
 
 
 
